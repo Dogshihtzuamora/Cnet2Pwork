97ada8cb7ca0a8491ee1c6aadb2f89ab0217b8e6
Ravy NS <148967139+Dogshihtzuamora@users.noreply.github.com>
2026-01-14 22:31:43 -0300

    HTML Index
    Updated file serving path and removed static middleware.

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ const path = require('path')
 const app = express()
 const server = http.createServer(app)
 
-app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 const wss = new WebSocket.Server({ server })
